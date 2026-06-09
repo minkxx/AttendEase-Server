f80fe2255895667d6cb20b45ca14859055295b44
Monsur Mazumder <monsurcodes@gmail.com>
2026-06-09 07:27:48 +0530

    feat: add CreateUserDto

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,14 @@
-export class CreateUserDto {}
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty({ message: 'Name should not be empty' })
+  name!: string;
+
+  @IsEmail({}, { message: 'Please provide a valid email address' })
+  email!: string;
+
+  @IsString()
+  @IsNotEmpty()
+  passwordHash!: string;
+}
