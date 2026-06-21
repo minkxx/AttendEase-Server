6a13711371eb2d6b4a79c3d739b617d827c784a1
Minkxx <monsurcodes@gmail.com>
2026-06-21 13:03:35 +0530

    refactor: use database connection instead of explicit prisma service

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,10 +1,13 @@
-import { Injectable } from '@nestjs/common';
-import { PrismaService } from '../prisma/prisma.service';
+import { Inject, Injectable } from '@nestjs/common';
 import { type UserSession } from '@thallesp/nestjs-better-auth';
+import { PrismaClient } from '../common/generated/prisma/client';
+import { DATABASE_CONNECTION } from '../database/database-connection';
 
 @Injectable()
 export class UserService {
-  constructor(private prismaService: PrismaService) {}
+  constructor(
+    @Inject(DATABASE_CONNECTION) private prismaService: PrismaClient,
+  ) {}
 
   getProfile(session: UserSession) {
     return session.user;
